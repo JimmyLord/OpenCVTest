6af8f80ffc9a7bb94d26e3cbfd3a9d089837954e
Jimmy Lord <jimmylord@gmail.com>
2019-09-11 13:51:29 -0400

    Added a threshhold test.

--- OpenCVTest/Source/WinMain.cpp
+++ OpenCVTest/Source/WinMain.cpp
@@ -10,13 +10,15 @@ void TestSimpleModification();
 void TestDFT();
 void TestVideo();
 void TestMorph();
+void TestThreshhold();
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
     //TestSimpleModification();
     //TestDFT();
     //TestVideo();
-    TestMorph();
+    //TestMorph();
+    TestThreshhold();
 
     waitKey();
 }
@@ -100,7 +102,7 @@ int const g_TBNumMorphTypes = 7; // Don't allow "hit or miss" for now.
 int g_TBMorphType = 0;
 int const g_TBNumKernelShapes = 3;
 int g_TBKernelShape = 0;
-int g_TBKernetSize = 0;
+int g_TBKernelSize = 0;
 int const g_MaxKernelSize = 21;
 void Morph(int, void*);
 
@@ -118,7 +120,7 @@ void TestMorph()
     // Select Kernel shape and size, call Erosion() every time they change.    
     createTrackbar( "Morph:", "Erosion Controls", &g_TBMorphType, g_TBNumMorphTypes-1, Morph ); //\n 0: Rect \n 1: Cross \n 2: Ellipse
     createTrackbar( "Shape:", "Erosion Controls", &g_TBKernelShape, g_TBNumKernelShapes-1, Morph ); //\n 0: Rect \n 1: Cross \n 2: Ellipse
-    createTrackbar( "Size:", "Erosion Controls", &g_TBKernetSize, g_MaxKernelSize-1, Morph ); //\n 2n +1
+    createTrackbar( "Size:", "Erosion Controls", &g_TBKernelSize, g_MaxKernelSize-1, Morph ); //\n 2n +1
 
     // Display the original and call Erosion() to display the initial result.
     ShowImageWithFixedWidthAtPosition( "Original", g_MorphSrc, 450, 50, 50 );
@@ -158,12 +160,58 @@ void Morph(int, void*)
     else if( g_TBKernelShape == 2 ) { kernelShape = MORPH_ELLIPSE; }
 
     // Create the kernel.
-    Mat kernel = getStructuringElement( kernelShape, Size( 2*g_TBKernetSize+1, 2*g_TBKernetSize+1 ), Point( g_TBKernetSize, g_TBKernetSize ) ) * 255.0f;
+    Mat kernel = getStructuringElement( kernelShape, Size( 2*g_TBKernelSize+1, 2*g_TBKernelSize+1 ), Point( g_TBKernelSize, g_TBKernelSize ) ) * 255.0f;
 
     // Apply the morphological transformation.
     morphologyEx( g_MorphSrc, g_MorphDst, morphType, kernel );
 
     // Display result and kernel.
-    ShowImageWithFixedWidthAtPosition( "Eroded", g_MorphDst, 450, 50 + 450+10, 50 );
+    ShowImageWithFixedWidthAtPosition( "Output", g_MorphDst, 450, 50 + 450+10, 50 );
     ShowImageWithFixedWidthAtPosition( "Kernel", kernel, 450, 50 + (450+10)*2, 50 );
+}
+
+// Globals for Trackbar settings.
+Mat g_ThreshholdSrc, g_ThreshholdDst;
+int const g_TBNumThreshholdTypes = 5; // Don't allow "hit or miss" for now.
+int g_TBThreshholdType = 0;
+int g_TBThreshholdValue = 0;
+void Threshhold(int, void*);
+
+void TestThreshhold()
+{
+    // Load the original image.
+    //g_ThreshholdSrc = imread( "Data/adult-city-female-1574140.jpg", IMREAD_GRAYSCALE );
+    g_ThreshholdSrc = imread( "Data/action-adventure-backlit-209209.jpg", IMREAD_GRAYSCALE );
+
+    // Create a window for erosion kernel settings.
+    namedWindow( "Threshhold Controls", WINDOW_KEEPRATIO );
+    resizeWindow( "Threshhold Controls", 450, 100 );
+    moveWindow( "Threshhold Controls", 50 + (450+10)*2, 550 );
+
+    // Select Threshhold type and max value, call Threshhold() every time they change.    
+    createTrackbar( "Type:", "Threshhold Controls", &g_TBThreshholdType, g_TBNumThreshholdTypes-1, Threshhold );
+    createTrackbar( "Value:", "Threshhold Controls", &g_TBThreshholdValue, 255, Threshhold );
+
+    // Display the original and call Erosion() to display the initial result.
+    ShowImageWithFixedWidthAtPosition( "Original", g_ThreshholdSrc, 450, 50, 50 );
+    Threshhold( 0, 0 );
+}
+
+void Threshhold(int, void*)
+{
+    int threshholdType = THRESH_BINARY;
+    if( g_TBThreshholdType == 0 )        { threshholdType = THRESH_BINARY; }
+    else if( g_TBThreshholdType == 1 )   { threshholdType = THRESH_BINARY_INV; }
+    else if( g_TBThreshholdType == 2 )   { threshholdType = THRESH_TRUNC; }
+    else if( g_TBThreshholdType == 3 )   { threshholdType = THRESH_TOZERO; }
+    else if( g_TBThreshholdType == 4 )   { threshholdType = THRESH_TOZERO_INV; }
+    //else if( g_TBThreshholdType == 5 )   { threshholdType = THRESH_MASK; }
+
+    // Apply the threshold.
+    threshold( g_ThreshholdSrc, g_ThreshholdDst, g_TBThreshholdValue, 255, threshholdType );
+    //threshold( g_ThreshholdSrc, g_ThreshholdDst, g_TBThreshholdValue, 255, threshholdType | THRESH_OTSU );
+    //threshold( g_ThreshholdSrc, g_ThreshholdDst, g_TBThreshholdValue, 255, threshholdType | THRESH_TRIANGLE );
+
+    // Display result and kernel.
+    ShowImageWithFixedWidthAtPosition( "Output", g_ThreshholdDst, 450, 50 + 450+10, 50 );
 }
